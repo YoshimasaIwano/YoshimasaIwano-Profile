7f802e94b4b49df2053e3d9de091447e696f27bc
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-06-04 00:34:04 -0700

    starting to create main page

---
+++ my-app/src/MainPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+/*  
+    MainPage Component
+*/
+
+export function MainPage(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+            </header>
+        </div>
+    );
+}
