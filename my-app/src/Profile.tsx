6f69cd2aed451aad815abca0526c0c536b315725
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-06-06 15:03:25 -0700

    working on project section

--- my-app/src/Profile.tsx
+++ my-app/src/Profile.tsx
@@ -26,14 +26,14 @@ export function Profile() {
                     I'm currently pursuing a Master's degree in Computer Science from San Francisco State University.</p>
                 <p className='tl ml3 mv4 f3'>
                     I am interested in Computer Vision and Software Development.<br/>
-                    In my undergraduate, I was in Visual Media Laboratory at University of Tsukuba. I was doing researh on medical imaging. Here's a 
+                    In my undergraduate, I was in Visual Media Laboratory at University of Tsukuba in Japan. I was doing researh on medical imaging. Here's a 
                     <a className='no-underline b dark-green hover-light-yellow' href='https://github.com/YoshimasaIwano/graduation-reaserach-on-lymph-tumor/blob/main/graduate_research_final.pdf'> paper.</a><br/>
-                    I also have some experience of Software Development at SFSU. The detai is in Project section.<br/>
+                    I also have some experience of Software Development at SFSU. The detail is in Project section.<br/>
                     I am familiar with technologies such as Pytorch, Tensorflow, Numpy, Pandas, Scikit-Learn, React, and Node.js
                 </p>
                 <p className='tl ml3 mv4 f3'>
-                    As a fast learner and a motivated person, I can adapt to any challenges without problems.<br/>
-                    I went through and overcame some difficult situations with a resolute and invincible sprit, so I am well-prepared to tackle challenges and maximie my ability.
+                    As a fast learner and a motivated person, I can adapt to any changes without problems.<br/>
+                    I went through and overcame some difficult situations with a resolute and invincible sprit, so I am well-prepared to tackle challenges and maximize my ability.
                 </p>
             </div>
         </div>
--- my-app/src/Project.tsx
+++ my-app/src/Project.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export function Project() {
     return (
-        <div>
-            Project!!!
+        <div id="project-container" className='pv2 tc f1'>
+            <h5 className='tl ml1 mv1'>Project</h5>
+            <div id="project-cell-container" className='pv2 flex flex-wrap items-center justify-center'>
+
+            </div>
         </div>
     )
 }
