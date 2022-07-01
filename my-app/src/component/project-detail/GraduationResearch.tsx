4cb2cd9b7e548e6a0827e51a73321880a1d6a737
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-07-01 15:03:56 -0700

    tried to use Link

--- my-app/src/component/project-detail/GraduationResearch.tsx
+++ my-app/src/component/project-detail/GraduationResearch.tsx
@@ -1,5 +1,6 @@
 import BackgroundPurpose from '../../assets/graduation-research-img/background-purpose.png';
 import Ensemble from '../../assets/graduation-research-img/ensemble.png';
+import { Link } from 'react-scroll';
 
 /*
     Graduation Research Detail Component  
@@ -9,7 +10,36 @@ export function GraduationResearch() {
     return (
         <>
             <div className='tl f2 b ml1 mv1'>Graduation Research</div>
-            <div className='db ma mh1 mv3 f3 b dark-gray w-80'>Purpose & Background</div>
+            <ul>
+                <li className='pointer'>
+                    <Link 
+                        activeClass='active'
+                        to="purposeAndBackground" 
+                        spy={true} 
+                        smooth={true}
+                        offset={-10}
+                        duration={2}
+                        >
+                        Purpose & Background
+                    </Link>
+                </li>
+                <li className='pointer'>
+                    <Link 
+                        activeClass='active'
+                        to="method" 
+                        spy={true} 
+                        smooth={true}
+                        offset={-10}
+                        duration={2}
+                        >
+                        Method
+                    </Link>
+                </li>
+                <li><Link to="Tools" spy={true} smooth={true}>Tools</Link></li>
+                <li><Link to="Results" spy={true} smooth={true}>Results</Link></li>
+                <li><Link to="Difficulties" spy={true} smooth={true}>Difficulties</Link></li>
+            </ul>
+            <div className='db ma mh1 mv3 f3 b dark-gray w-80' id="purposeAndBackground">Purpose & Background</div>
             <div className='db ma flex-row flex-nowrap justify-items-center tc w-80'>
                 <p className='tl mh2 mv2 f3'>
                     There are two types of a residual tumor; teratoma and necrotic tissue. 
@@ -27,7 +57,7 @@ export function GraduationResearch() {
                     The purpose of my research is to get a similar or better result of them by ensembling CNN and SVM.
                 </p>
             </div>
-            <div className='db ma mh1 mv3 f3 b dark-gray w-80'>Method</div>
+            <div className='db ma mh1 mv3 f3 b dark-gray w-80' id="method">Method</div>
             <div className='db ma flex-row tc w-80'>
                 <p className='tl mh2 mv2 f3'>
                     We used ensemble model consisting of 3 of SVMs, Resnet50, and Resnet152.<br />
@@ -36,20 +66,20 @@ export function GraduationResearch() {
                 </p>
                 <img src={Ensemble} className="db ma mv3 w-50 h-50"></img>
             </div>
-            <div className='db ma mh1 mv3 f3 b dark-gray w-80'>Tools</div>
+            <div className='db ma mh1 mv3 f3 b dark-gray w-80' id="Tools">Tools</div>
             <div className='dt ma flex-row tc w-80'>
                 {ToolList.map((tool) => {
                     return <ToolContent name={tool.name} description={tool.description} />
                 })}
             </div>
-            <div className='db ma mh1 mv3 f3 b dark-gray w-80'>Results</div>
+            <div className='db ma mh1 mv3 f3 b dark-gray w-80' id="Results">Results</div>
             <div className='db ma flex-row flex-nowrap justify-items-center tc w-80'>
                 <p className='tl mh2 mv2 f3'>
                     We got a similar result as previous studies; accuracy is 78.3%, sensitivity is 61.3%, and specificity is 90.6%, 
                     which improved comparing to a single Resnet model.
                 </p>
             </div>
-            <div className='db ma mh1 mv3 f3 b dark-gray w-80'>Difficulties</div>
+            <div className='db ma mh1 mv3 f3 b dark-gray w-80' id="Difficulties">Difficulties</div>
             <div className='db ma flex-row flex-nowrap justify-items-center tc w-80'>
                 <p className='tl mh2 mv2 f3'>
                     ・Lack of CT images: <br/>
@@ -121,3 +151,28 @@ function ToolContent({ name, description }: ToolProps): JSX.Element {
         </div>
     )
 } 
+
+function PurposeAndBackground(): JSX.Element {
+    return (
+        <div id="purpose">
+            <div className='db ma mh1 mv3 f3 b dark-gray w-80' id="Purpose & Background">Purpose & Background</div>
+            <div className='db ma flex-row flex-nowrap justify-items-center tc w-80'>
+                <p className='tl mh2 mv2 f3'>
+                    There are two types of a residual tumor; teratoma and necrotic tissue. 
+                    Teratoma is malignant and needs to be excised. However, necrotic tissue is benign and a surgery is an overtreatment. 
+                    In addition, The conventional biomarker can not identify the teratoma before a surgery.
+                    Therefore, we aimed to predict the pathology of postchemotherapy residual tumors using CNN and SVM.
+                </p>
+                <img src={BackgroundPurpose} className="db ma mv3 w-50 h-50"></img>
+                <p className='tl mh2 mv2 f3'>
+                    I found two previous studies on the exact same topic.
+                    <a className='no-underline b black hover-light-yellow' href='https://ascopubs.org/doi/full/10.1200/CCI.18.00004'> The one </a>
+                    is published by Jeremy Lewin, etc. They used SVM and got accuracy: 72.0%, sensitivity: 56.2%, specificity: 81.9% <br />
+                    <a className='no-underline b black hover-light-yellow' href='https://link.springer.com/article/10.1007/s00330-019-06495-z?wt_mc=Internal.Event.1.SEM.ArticleAuthorOnlineFirst&utm_source=ArticleAuthorOnlineFirst&utm_medium=email&utm_content=AA_en_06082018&ArticleAuthorOnlineFirst_20191212'> The another one </a>
+                    is published by Bettina Baessler, etc. They used Gradient-boosted trees and got accuracy: 81%, sensitivity: 89%, specificity: 72% <br />
+                    The purpose of my research is to get a similar or better result of them by ensembling CNN and SVM.
+                </p>
+            </div>
+        </div>
+    )
+}
