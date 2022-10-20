bb085bfce39c65e42187ae6844f5ace3a44b946b
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-10-20 02:27:46 -0700

    start to work on adding mysql

---
+++ my-app/src/entity/Flower.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Flower {
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+
+    @Column("varchar", {length: 255})
+    src: string;
+
+    @Column("varchar", {length: 45})
+    color: string;
+
+    @Column("varchar", {length: 45})
+    keywords: string;
+
+    @Column("varchar", {length: 255})
+    description: string;
+}
